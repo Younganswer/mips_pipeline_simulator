6f66ef27007c989394d2b6ae7cd56481797613da
Younganswer <dudtj1999@gmail.com>
2022-11-21 12:53:35 +0900

    .

--- incs/camera.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CAMERA_HPP
-# define CAMERA_HPP
-
-# include "../raylib/src/raylib.h"
-
-bool	init_camera(Camera2D &camera);
-bool	manipulate_camera(Camera2D &camera);
-
-#endif
--- srcs/window/draw_window.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../incs/window.hpp"
-
-bool	draw_window(Info &info) {
-	draw_camera_sight(info);
-    draw_guide();
-	DrawTextureRec(info.button.button, info.button.sourceRec, (Vector2){ info.button.btnBounds.x, info.button.btnBounds.y }, WHITE);
-	draw_edge();
-	return (true);
-}
