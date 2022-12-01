054b1410ef4599945297d8e3fe4a7b5e2c180e3c
Younganswer <dudtj1999@gmail.com>
2022-12-01 16:45:00 +0900

    .

--- srcs/shape/draw_arrow.cpp
+++ srcs/shape/draw_arrow.cpp
@@ -8,7 +8,7 @@ bool	draw_down_arrow(float startPosX, float startPosY, float endPosX, float endP
 bool	draw_left_arrow(float startPosX, float startPosY, float endPosX, float endPosY, Color color) {
 	DrawLineEx((Vector2){startPosX, startPosY}, (Vector2){endPosX, endPosY}, 5.0f, color);
 	DrawLineEx((Vector2){endPosX, endPosY}, (Vector2){endPosX+ARROW_LENGTH, endPosY+ARROW_LENGTH}, 5.0f, color);
-DrawLineEx((Vector2){endPosX, endPosY}, (Vector2){endPosX+ARROW_LENGTH, endPosY-ARROW_LENGTH}, 5.0f, color);
+	DrawLineEx((Vector2){endPosX, endPosY}, (Vector2){endPosX+ARROW_LENGTH, endPosY-ARROW_LENGTH}, 5.0f, color);
 	return (true);
 }
 
