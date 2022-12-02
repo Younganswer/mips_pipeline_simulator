81d1999d78cc318fbee5ccf652a8c66fd33c8e6a
Younganswer <dudtj1999@gmail.com>
2022-12-02 22:57:45 +0900

    .

--- incs/shape.hpp
+++ incs/shape.hpp
@@ -12,6 +12,9 @@ bool	draw_right_arrow(float startPosX, float startPosY, float endPosX, float end
 bool	draw_up_arrow(float startPosX, float startPosY, float endPosX, float endPosY, Color color);
 bool	draw_down_arrow(float startPosX, float startPosY, float endPosX, float endPosY, Color color);
 
+// draw_circle_sector_lines_ex.cpp
+bool	draw_circle_sector_lines_ex(float centerX, float centerY, float radius, float startAngle, float endAngle, int segments, float thick, Color color);
+
 // draw_mux.cpp
 bool	draw_mux_0_1(int posX, int posY, Color color);
 bool	draw_mux_1_0(int posX, int posY, Color color);
