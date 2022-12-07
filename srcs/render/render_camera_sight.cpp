4a5833adb335e64d1f69dc62c9372b8eba8bdee0
Younganswer <dudtj1999@gmail.com>
2022-12-07 16:32:05 +0900

    .

--- srcs/render/render_camera_sight.cpp
+++ srcs/render/render_camera_sight.cpp
@@ -14,25 +14,25 @@ bool	render_camera_sight(Info &info) {
 	BeginMode2D(info.camera);
 		
 		// check instruction --------------------------------------------------------------------------
-			for (auto iter=info.instructions.begin(); iter!=info.instructions.end(); iter++) {
-				string	cur_str = iter->get_asm_string();
-				cur_str += " " + to_string(iter->get_opcode());
-				if (iter->get_format() == R) {
-					cur_str += " " + to_string(iter->get_rs());
-					cur_str += " " + to_string(iter->get_rt());
-					cur_str += " " + to_string(iter->get_rd());
-					cur_str += " " + to_string(iter->get_shamt());
-					cur_str += " " + to_string(iter->get_funct());
-				} else if (iter->get_format() == I) {
-					cur_str += " " + to_string(iter->get_rs());
-					cur_str += " " + to_string(iter->get_rt());
-					cur_str += " " + to_string(iter->get_imm());
-				} else if (iter->get_format() == J) {
-					cur_str += " " + to_string(iter->get_imm());
-				}
-				DrawText(cur_str.c_str(), posX-500, posY+offsetY, 20, RED);
-				offsetY += 30;
-			}
+			// for (auto iter=info.instructions.begin(); iter!=info.instructions.end(); iter++) {
+			// 	string	cur_str = iter->get_asm_string();
+			// 	cur_str += " " + to_string(iter->get_opcode());
+			// 	if (iter->get_format() == R) {
+			// 		cur_str += " " + to_string(iter->get_rs());
+			// 		cur_str += " " + to_string(iter->get_rt());
+			// 		cur_str += " " + to_string(iter->get_rd());
+			// 		cur_str += " " + to_string(iter->get_shamt());
+			// 		cur_str += " " + to_string(iter->get_funct());
+			// 	} else if (iter->get_format() == I) {
+			// 		cur_str += " " + to_string(iter->get_rs());
+			// 		cur_str += " " + to_string(iter->get_rt());
+			// 		cur_str += " " + to_string(iter->get_imm());
+			// 	} else if (iter->get_format() == J) {
+			// 		cur_str += " " + to_string(iter->get_imm());
+			// 	}
+			// 	DrawText(cur_str.c_str(), posX-500, posY+offsetY, 20, RED);
+			// 	offsetY += 30;
+			// }
 		// check instruction -------------------------------------------------------------------------
 
 		// cross line --------------------------------------------------------------------------------
