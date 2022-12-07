a83c6bee3c6ea0a4609d4e340adc8060c79495f1
jasong <jasong@student.42seoul.kr>
2022-12-07 09:14:40 +0900

    .

--- srcs/stages/execute.cpp
+++ srcs/stages/execute.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 bool	execute(Info &info) {
 	cout << "execute" << '\n';
+	// set control signal in EX/MEM pipeline register
+	info.exmem.set_mem_to_reg(info.idex.get_mem_to_reg());
+	info.exmem.set_reg_write(info.idex.get_reg_write());
+	info.exmem.set_mem_read(info.idex.get_mem_read());
+	
+
+
+
 	(void) info;
 	return (true);
 }
