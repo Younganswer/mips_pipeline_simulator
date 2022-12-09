008d806d27cf3b0c93230baf6e2c6b5cc1d493ac
jasong <jasong@student.42seoul.kr>
2022-12-09 19:17:01 +0900

    execute

--- srcs/forward/forward.cpp
+++ srcs/forward/forward.cpp
@@ -5,8 +5,8 @@ Forward::Forward(void) {
 	this->exRt = 0;
 	this->memRd = 0;
 	this->wbRd = 0;
-	this->memWb = false;
-	this->wbWb = false;
+	this->memRw = false;
+	this->wbRw = false;
 }
 
 Forward::~Forward(void) {}
@@ -16,31 +16,32 @@ int		Forward::get_ex_rs(void) const { return this->exRs; }
 int		Forward::get_ex_rt(void) const { return this->exRt; }
 int		Forward::get_mem_rd(void) const { return this->memRd; }
 int		Forward::get_wb_rd(void) const { return this->wbRd; }
-bool	Forward::get_mem_wb(void) const { return this->memWb; }
-bool	Forward::get_wb_wb(void) const { return this->wbWb; }
+bool	Forward::get_mem_rw(void) const { return this->memRw; }
+bool	Forward::get_wb_rw(void) const { return this->wbRw; }
 
 // Setter
 void	Forward::set_ex_rs(int exRs) { this->exRs = exRs; }
 void	Forward::set_ex_rt(int exRt) { this->exRt = exRt; }
 void	Forward::set_mem_rd(int memRd) { this->memRd = memRd; }
 void	Forward::set_wb_rd(int wbRd) { this->wbRd = wbRd; }
-void	Forward::set_mem_wb(bool memWb) { this->memWb = memWb; }
-void	Forward::set_wb_wb(bool wbWb) { this->wbWb = wbWb; }
+void	Forward::set_mem_rw(bool memRw) { this->memRw = memRw; }
+void	Forward::set_wb_rw(bool wbRw) { this->wbRw = wbRw; }
 
 // Utils
+
 int		Forward::forward_a(void) const {
-	if (this->exRs == this->wbRd && this->wbWb) {
+	if (this->exRs == this->wbRd && this->wbRw) {
 		return (2);
-	} else if (this->exRs == this->memRd && this->memWb) {
+	} else if (this->exRs == this->memRd && this->memRw) {
 		return (1);
 	}
 	return (0);
 }
 
 int		Forward::forward_b(void) const {
-	if (this->exRt == this->wbRd && this->wbWb) {
+	if (this->exRt == this->wbRd && this->wbRw) {
 		return (2);
-	} else if (this->exRt == this->memRd && this->memWb) {
+	} else if (this->exRt == this->memRd && this->memRw) {
 		return (1);
 	}
 	return (0);
--- srcs/stages/execute.cpp
+++ srcs/stages/execute.cpp
@@ -10,12 +10,58 @@ bool	execute(Info &info) {
 		info.exmem.set_mem_to_reg(info.idex.get_mem_to_reg());
 		info.exmem.set_reg_write(info.idex.get_reg_write());
 	// set control signal in EX/MEM pipeline register -------------------------------------------------------------
+	// check forwarding ------------------------------------------------------------------------------------------
+		// set forwarding values in EX/MEM pipeline register -------------------------------------------------------
+		info.forward.set_wb_rd(info.memwb.get_instruction().get_rd());
+		info.forward.set_wb_rw(info.memwb.get_reg_write());
+		info.forward.set_mem_rd(info.exmem.get_instruction().get_rd());
+		info.forward.set_ex_rs(info.idex.get_instruction().get_rs());
+		info.forward.set_ex_rt(info.idex.get_instruction().get_rt());
+		// set forwarding values in EX/MEM pipeline register -------------------------------------------------------
 
-	// TODO: select rs value in mux - is it forwarded or not? -----------------------------------------------------
+	// TODO: select read data 2 value in mux - is it forwarded or not? -----------------------------------------------------
+	ui	aluInputa = 0;
+	if (info.forward.forward_a() == 0) {
+		aluInputa = info.idex.get_read_data1();
+	} else if (info.forward.forward_a() == 1) {
+		aluInputa = info.exmem.get_alu_result();
+	} else if (info.forward.forward_a() == 2) {
+		aluInputa = info.memwb.get_alu_result();
+	}
+	ui	aluInputb = 0;
+	if (info.forward.forward_b() == 0) {
+		aluInputb = info.idex.get_read_data2();
+	} else if (info.forward.forward_b() == 1) {
+		aluInputb = info.exmem.get_alu_result();
+	} else if (info.forward.forward_b() == 2) {
+		aluInputb = info.memwb.get_alu_result();
+	}
+	// calculate alu control
+	ui	aluControl = calc_alu_control(info.idex.get_alu_op(), info.idex.get_instruction().get_funct());
+	// calculate ALU result by ALU control signal ----------------------------------------------------------------
+	ui	aluResult = 0;
+	if (aluControl == 0b0010) { // ADD
+		aluResult = aluInputa + aluInputb;
+	} else if (aluControl == 0b0110) { // SUB
+		aluResult = aluInputa - aluInputb;
+	} else if (aluControl == 0b0000) { // AND
+		aluResult = aluInputa & aluInputb;
+	} else if (aluControl == 0b0001) { // OR
+		aluResult = aluInputa | aluInputb;
+	} else if (aluControl == 0b0111) { // SLT
+		aluResult = aluInputa < aluInputb;
+	}
+	(void)aluResult;
+	// calculate ALU result by ALU control signal ----------------------------------------------------------------
 	// TODO: select rt value in mux - is it forwarded or not? -----------------------------------------------------
 	// TODO: select ALU Source value in mux - is it rt value or immediated value? ---------------------------------
 	// TODO: Calculate ALU result ---------------------------------------------------------------------------------
 	// TODO: select register destination - is it rd or rt? --------------------------------------------------------
+	ui	regDst = 0;
+	if (info.idex.get_reg_dst() == 0)
+		regDst = info.idex.get_rt();
+	else if (info.idex.get_reg_dst() == 1)
+		regDst = info.idex.get_rd();
 	// TODO: set EX/MEM pipeline register values ------------------------------------------------------------------
 	(void) info;
 	return (true);
