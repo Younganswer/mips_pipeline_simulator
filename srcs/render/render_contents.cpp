48c6ed2b338d0ed03e48be9294ccf70004dde5da
Younganswer <dudtj1999@gmail.com>
2022-12-09 22:58:29 +0900

    .

--- srcs/render/render_contents.cpp
+++ srcs/render/render_contents.cpp
@@ -3,18 +3,18 @@
 
 bool	render_contents(Info &info) {
 	// draw operations ------------------------------------------------------------------
-		render_instruction_fetch(info);
-		render_instruction_decode(info);
-		render_execute(info);
-		render_memory_access(info);
 		render_write_back(info);
+		render_memory_access(info);
+		render_execute(info);
+		render_instruction_decode(info);
+		render_instruction_fetch(info);
 	// draw operations ------------------------------------------------------------------
 
 	// draw pipeline --------------------------------------------------------------------
-		render_if_id(info);
-		render_id_ex(info);
-		render_ex_mem(info);
 		render_mem_wb(info);
+		render_ex_mem(info);
+		render_id_ex(info);
+		render_if_id(info);
 	// draw pipeline --------------------------------------------------------------------
 
 	// draw units -----------------------------------------------------------------------
