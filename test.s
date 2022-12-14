b71701c334658fb444227143e89e5abaca151f4f
Younganswer <dudtj1999@gmail.com>
2022-12-14 23:53:18 +0900

    .

--- test.s
+++ test.s
@@ -24,22 +24,12 @@ var18:	.word 18
 .text 0x00400024
 .globl main
 
-foo:
-	nop
-	sw		$t2, 8($gp)
-	slt		$a0, $t0, $t1
-	sub		$t2, $t1, $t0
-	or		$t2, $t1, $t0
-	and		$t2, $t1, $t0
-	nop
-	nop
-
 main:
 	lw		$t0, 0($gp)
 	lw		$t1, 4($gp)
-	beq		$t0, $t1, foo
+	beq		$t0, $t1, bar
 	add		$t2, $t0, $t0
-	beq		$t2, $t1, foo
+	beq		$t2, $t1, bar
 	j		bar
 	add		$t3, $t2, $t1
 
